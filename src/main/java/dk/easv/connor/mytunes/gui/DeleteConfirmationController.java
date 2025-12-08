5b9b55ecc30885c04df0d74246695a1d8ac1d476
ConMcg2025 <cjmcgrail2011@hotmail.com>
2025-12-08 13:00:53 -0600

    added method to close the delete confirmation window

--- src/main/java/dk/easv/connor/mytunes/gui/DeleteConfirmationController.java
+++ src/main/java/dk/easv/connor/mytunes/gui/DeleteConfirmationController.java
@@ -1,8 +1,12 @@
 package dk.easv.connor.mytunes.gui;
 
+import dk.easv.connor.mytunes.be.Playlist;
+import dk.easv.connor.mytunes.be.Song;
+import dk.easv.connor.mytunes.bll.Logic;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class DeleteConfirmationController
 {
@@ -12,6 +16,16 @@ public class DeleteConfirmationController
     @FXML
     private Button deleteCancelButton;
 
+    private Logic logic;
+    private Song songToEdit;
+    private HelloController parentController;
+    private Playlist playlist;
+
+    public void initialize()
+    {
+        logic = new Logic();
+    }
+
     public void onDeleteConfirmButtonClick(ActionEvent actionEvent)
     {
 
@@ -19,6 +33,7 @@ public class DeleteConfirmationController
 
     public void onDeleteCancelButtonClick(ActionEvent actionEvent)
     {
-
+        Stage stage = (Stage) deleteCancelButton.getScene().getWindow();
+        stage.close();
     }
 }
